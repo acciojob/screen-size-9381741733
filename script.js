74706bcca91627b2897f998f6151fdb2deea98f2
jyothsna bhukya <jyothsnabhukya022@gmail.com>
2025-03-16 17:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    document.getElementById("sizeInfo").innerHTML = `Width: ${width} and Height: ${height}`;
+}
+
+window.addEventListener("resize", updateSize);
+updateSize(); // Call once to initialize
+
